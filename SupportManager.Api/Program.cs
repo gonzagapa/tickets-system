bf608e2e1ca8fd576b0f90a741453b262b03e74f
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2026-07-30 23:17:53 -0600

    chore: configure scalar api documentation.

--- SupportManager.Api/Program.cs
+++ SupportManager.Api/Program.cs
@@ -1,11 +1,25 @@
+using Scalar.AspNetCore;
 using SupportManager.Api.Services;
 using SupportManager.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document,context, cancellatin) =>
+    {
+        document.Info.Version = "1.0";
+        document.Info.Title = "Ticket System API";
+        document.Info.Description = "This API uses N-Layers infraestructure (Data,API, Web)";
+        document.Info.Contact = new Microsoft.OpenApi.OpenApiContact
+        {
+          Name = "Gonzalo Perez",
+          Email = "gonzalo.anuar13@outlook.com"  
+        };
+        return Task.CompletedTask;
+    });
+});
 builder.Services.AddControllers(); //register controllers
 
 builder.Services.AddScoped<ITicketRepositories, TicketRepository>();
@@ -17,6 +31,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
